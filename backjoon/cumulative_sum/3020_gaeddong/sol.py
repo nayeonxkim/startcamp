acb6f521f9aa94a4b43148dc6911261dfa7848eb
NayeonKim <kny0628m@gmail.com>
2023-03-09 17:58:45 +0900

    +

--- backjoon/cumulative_sum/3020_gaeddong/sol.py
+++ backjoon/cumulative_sum/3020_gaeddong/sol.py
@@ -3,3 +3,11 @@ sys.stdin = open('input.txt')
 
 N, H = map(int, sys.stdin.readline().split())
 
+upper = [0] * (N//2)
+lower = [0] * (N//2)
+
+for i in range(N//2):
+    lower[i] = int(sys.stdin.readline())
+    upper[i] = int(sys.stdin.readline())
+
+print(lower, upper)
