897e98f830fd765d984ec4b39b6aa7d8cb277cbe
NayeonKim <kny0628m@gmail.com>
2023-03-13 22:47:23 +0900

    개똥

--- backjoon/cumulative_sum/3020_gaeddong/sol.py
+++ backjoon/cumulative_sum/3020_gaeddong/sol.py
@@ -1,13 +1,31 @@
 import sys
 sys.stdin = open('input.txt')
 
+# N, H = map(int, sys.stdin.readline().split())
+#
+# upper = [0] * N
+# lower = [0] * N
+#
+# for _ in range(N//2):
+#     l = int(sys.stdin.readline())
+#     u = int(sys.stdin.readline())
+#     lower[l] = upper[u] = 1
+#
+# print(lower, upper)
+
+
 N, H = map(int, sys.stdin.readline().split())
+lst = [0] * H
 
-upper = [0] * (N//2)
-lower = [0] * (N//2)
+for _ in range(N//2):
+    A = int(sys.stdin.readline())
+    B = int(sys.stdin.readline())
 
-for i in range(N//2):
-    lower[i] = int(sys.stdin.readline())
-    upper[i] = int(sys.stdin.readline())
+    for i in range(H-1, H-A-1, -1):
+        lst[i] += 1
 
-print(lower, upper)
+    for i in range(B):
+        lst[i] += 1
+print(lst)
+minV = min(lst)
+print(minV, lst.count(minV))
