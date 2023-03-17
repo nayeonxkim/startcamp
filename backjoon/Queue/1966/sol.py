2880ac13bf2d992eb2ede2d3eb4a171d943d7608
nayeonkim <kny0628m@gmail.com>
2023-03-17 17:59:36 +0900

    230317 queue

---
+++ backjoon/Queue/1966/sol.py
@@ -0,0 +1,34 @@
+'''
+1. 리스트 enumarate로 받기 : 인덱스 붙여서 쌍으로 확인
+6개, 확인하고 싶은 문서 = 0번 문서
+"0, 1" (1, 1) (2, 9) (3, 1) (4, 1) (5, 1)
+(1, 1) (2, 9) (3, 1) (4, 1) (5, 1) "0, 1"
+(2, 9) (3, 1) (4, 1) (5, 1) "0, 1" (1, 1)
+
+2. queue[0][1]이 다른 모든 queue[i][1]보다 큰지 확인
+cnt = 0
+- for문?
+- max?
+
+있다
+    : queue.append(queue.pop(0))
+없다
+    : cnt += 1
+    if queue[0][0] == M:
+        break
+    else:
+        queue.pop(0)
+
+'''
+
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    queue = list(enumerate(map(int, input().split())))
+    print(queue)
+
+    print(max(queue))
+
